eae75528ce0fdcb9ca541e58506bade529129840
Jose Neto <jose.neto@compliancesolucoes.com.br>
2026-04-16 14:21:24 -0300

    QRCode e Password implementados

---
+++ src/services/password/handle.js
@@ -0,0 +1,37 @@
+async function permittedCharacters() {
+    let permitted = [];
+
+    if (process.env.UPPERCASE_LETTERS === "true") {
+        permitted.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+
+    if (process.env.LOWERCASE_LETTERS === "true") {
+        permitted.push(..."abcdefghijklmnopqrstuvwxyz");
+    }
+
+    if (process.env.NUMBERS === "true") {
+        permitted.push(..."0123456789");
+    }
+
+    if (process.env.SPECIAL_CHARACTERS === "true") {
+        permitted.push(..."!@#$%^&*()-_");
+    }
+
+    return permitted;
+}
+
+async function handle() {
+    let password = "";
+
+    const passwordLength = Number(process.env.PASSWORD_LENGTH);
+    const characters = await permittedCharacters();
+
+    for (let i = 0; i < passwordLength; i++) {
+        const index = Math.floor(Math.random() * characters.length);
+        password += characters[index];
+    }
+
+    return password;
+}
+
+export default handle;
